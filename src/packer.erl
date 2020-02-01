d037d01fac4627f4fea3848318111bc203c3d529
zack-bitcoin <zack.bitcoin@gmail.com>
2020-02-01 06:11:24 -0300

    add rng_confirm_tx to the white list

--- src/packer.erl
+++ src/packer.erl
@@ -39,6 +39,7 @@ is_b_atom(<<"rng_result_tx">>) -> true;
 is_b_atom(<<"rng_challenge_tx">>) -> true;
 is_b_atom(<<"rng_response_tx">>) -> true;
 is_b_atom(<<"rng_refute_tx">>) -> true;
+is_b_atom(<<"rng_confirm_tx">>) -> true;
 is_b_atom(<<"add">>) -> true;
 is_b_atom(<<"take">>) -> true;
 is_b_atom(<<"tx_scan">>) -> true;
